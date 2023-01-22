591f8fc5c5d1b8a9ac75e02828dc79fdb1f3948b
Timothy Peterson <33550033+tmrp@users.noreply.github.com>
2023-01-22 17:07:43 +0100

    Create f1-car-scene.js

---
+++ src/three/f1-car-scene.js
@@ -0,0 +1,103 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+let mixer;
+
+let model;
+
+const clock = new THREE.Clock();
+
+const container = document.getElementById('container');
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+
+renderer.setSize(container.offsetWidth, container.offsetHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+container.appendChild(renderer.domElement);
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+const scene = new THREE.Scene();
+
+scene.environment = pmremGenerator.fromScene(
+  new RoomEnvironment(),
+  0.04
+).texture;
+
+const camera = new THREE.PerspectiveCamera(
+  40,
+  container.offsetWidth / container.offsetHeight,
+  1,
+  100
+);
+camera.position.set(5, 4, 7);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0.5, 0);
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+const dracoLoader = new DRACOLoader();
+
+dracoLoader.setDecoderPath('jsm/libs/draco/gltf/');
+
+const loader = new GLTFLoader();
+
+const scaleFactor = (input, limit = false) => {
+  const scale = input / 1000;
+  if (limit && scale > 1) {
+    return 1;
+  }
+  return scale;
+};
+
+loader.setDRACOLoader(dracoLoader);
+loader.load(
+  '/3d/f1_2022_generic_free.glb',
+  (gltf) => {
+    model = gltf.scene;
+
+    const scale = scaleFactor(container.offsetWidth, true);
+
+    model.scale.set(scale, scale, scale);
+
+    scene.add(model);
+
+    mixer = new THREE.AnimationMixer(model);
+
+    animate();
+  },
+  undefined,
+  function (e) {
+    console.error(e);
+  }
+);
+
+window.onresize = function () {
+  camera.aspect = container.offsetWidth / container.offsetHeight;
+  camera.updateProjectionMatrix();
+
+  const scale = scaleFactor(container.offsetWidth, true);
+  model.scale.set(scale, scale, scale);
+
+  renderer.setSize(container.offsetWidth, container.offsetHeight);
+};
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  const delta = clock.getDelta();
+
+  mixer.update(delta);
+
+  controls.update();
+
+  model.rotation.y += 0.001;
+
+  renderer.render(scene, camera);
+}
